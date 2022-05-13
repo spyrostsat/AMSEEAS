9cf3667c601c31ea9c387baf31daffba191826ad
Spyros Tsattalios <85879294+spyrostsat@users.noreply.github.com>
2022-05-13 20:16:45 +0300

    Inserted a how-to-call-amseeas file

---
+++ Algorithms/AMSEEAS_call.py
@@ -0,0 +1,13 @@
+# How to call AMSEEAS
+# EXAMPLE ON SPHERE FUNCTION
+
+import amseeas
+
+def sphere(x):
+    fx = 0
+    for l in range(0, len(x)):
+        fx += x[l] ** 2
+    return fx
+
+
+a, b, c, d, e, f, g, h, i, j, k = amseeas.amseeas(15, 32, [-5.12, -5.12, -5.12, -5.12, -5.12, -5.12, -5.12, -5.12, -5.12, -5.12, -5.12, -5.12, -5.12, -5.12, -5.12], [5.12, 5.12, 5.12, 5.12, 5.12, 5.12, 5.12, 5.12, 5.12, 5.12, 5.12, 5.12, 5.12, 5.12, 5.12], sphere, 500)
